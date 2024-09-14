e7807c32fb71dfde3c00cbfbeac911a07b8d92cd
Алексей Денисенко <AlexeyDenisenko2703@yandex.ru>
2024-09-14 12:06:49 +0300

    add model product

--- products/models.py
+++ products/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 
+from products.validators import validate_min_value
+
 
 class Product(models.Model):
     """  Модель продукта. """
     name = models.CharField(max_length=200, verbose_name='Название товара')
-    description = models.TextField(verbose_name='Описание')
+    description = models.TextField(verbose_name='Описание', blank=True,
+                                   null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2,
-                                verbose_name='Цена')
+                                verbose_name='Цена',
+                                validators=[validate_min_value])
 
     def __str__(self):
         return self.name
